2854d5425bf233359faa3912a749ac29df82a309
Sergio Campos <morrodogg8@gmail.com>
2019-05-19 13:23:01 -0700

    Initial Commit

---
+++ e1.8/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  e1.8
+//
+//  Created by Sergio campos on 5/19/19.
+//  Copyright © 2019 Sergio campos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
